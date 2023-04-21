0c584280cba04d6e633e0ab6fa9c6e2e90f65cb2
Amir Ismayilov <118259339+Amir-Ismayilov@users.noreply.github.com>
2023-04-21 17:18:50 +0400

    Add project

---
+++ src/components/NewMemory.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Yeni Anı Componenti....</h1>
+    <p>Shows functionality keep-alive</p>
+    <button @click="counter++">PLUS +</button>
+    <p> {{ counter }}</p>
+    <p>check console</p>
+  </div>
+</template>
+<script>
+  export default {
+    data : function(){
+      return {
+        counter : 0
+      }
+    },
+    destroyed(){
+      console.log("Destroyed!!!");
+    },
+    activated(){
+      console.log("Activated!!!")
+    },
+    deactivated(){
+      console.log("DeActivated!!!")
+    }
+  }
+</script>
